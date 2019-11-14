5a18e5ca9da9424b3b20593d97ca6e605af3231c
Ymalaca <wendellifal@gmail.com>
2019-11-14 15:59:55 -0300

    Envio dos layouts - Wendell

---
+++ app/src/main/java/com/ifal/wendell/bookingnow/ActivityAdicionarReserva.java
@@ -0,0 +1,18 @@
+package com.ifal.wendell.bookingnow;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ALUNO on 14/11/2019.
+ */
+
+public class ActivityAdicionarReserva extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_reserva);
+    }
+}
