d149ae55592690d61e59960fbd6a56b25b7ee3b2
fabriciaoliveira <oliveirafabricia.2000@gmail.com>
2019-11-18 19:03:09 -0200

    Adição dos layouts de formulário e menus

--- app/src/main/java/com/ifal/wendell/bookingnow/MainActivity.java
+++ app/src/main/java/com/ifal/wendell/bookingnow/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.adicionarRestaurante) {
+        if (id == R.id.VerTotalReservas) {
             return true;
         }
 
